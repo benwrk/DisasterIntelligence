925ae4b4025f78ca77fa461f28151b08730e3178
Kasidit Phoncharoen <jubwarata@gmail.com>
2016-10-09 08:47:16 +0700

    fix index

--- DisasterIntelligence/app/views.py
+++ DisasterIntelligence/app/views.py
@@ -3,6 +3,7 @@ Definition of views.
 """
 import urllib.request
 import xml.etree.ElementTree
+from math import radians, sin, atan2, sqrt, cos
 
 from django.shortcuts import render
 from django.http import HttpRequest
@@ -33,15 +34,21 @@ def home(request):
 def get_lat_lon(request):
     f = urllib.request.urlopen(seismic_feed_api).read()
     root = xml.etree.ElementTree.fromstring(f)
-    return {'result': True, 'conv': conv(root)}
+    distance = request.GET.get('distance')
+    print(request.GET)
+    lat = request.GET.get('lat')
+    print(lat)
+    log = request.GET.get('lon')
+    print(log)
+    return {'result': True, 'conv': conv(root, distance, lat, log)}
 
 
-def conv(xmlroot):
+def conv(xmlroot, distance, lat, log):
     r = []
     z_index = 0
     for i in xmlroot:
         if i.tag == 'DailyEarthquakes':
-            e = [None, None, None, None]
+            e = [None, None, None, None, None, None, None, None]
             for j in i:
                 if j.tag == 'OriginEnglish':
                     e[0] = j.text
@@ -49,7 +56,37 @@ def conv(xmlroot):
                     e[1] = j.text
                 elif j.tag == 'Longitude':
                     e[2] = j.text
-            e[3] = z_index
-            z_index += 1
-            r.insert(len(r), e)
+                elif j.tag == 'DateTimeThai':
+                    e[7] = j.text
+                    print(e[3])
+                elif j.tag == 'Depth':
+                    e[4] = j.text
+                elif j.tag == 'Magnitude':
+                    e[5] = j.text
+                elif j.tag == 'TitleThai':
+                    e[6] = j.text
+            if float(distance) >= calculate_distance(lat, log, e[1], e[2]):
+                e[3] = z_index
+                z_index += 1
+                r.insert(len(r), e)
     return r
+
+
+def calculate_distance(tlat1, tlon1, tlat2, tlon2):
+    R = 6373.0
+
+    lat1 = radians(float(tlat1))
+    lon1 = radians(float(tlon1))
+    lat2 = radians(float(tlat2))
+    lon2 = radians(float(tlon2))
+
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+
+    a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
+    c = 2 * atan2(sqrt(a), sqrt(1 - a))
+
+    distance = R * c
+
+    print("Result:", distance)
+    return distance
